58676c29cf2bf32b69bdaf41d04da845bcf3fa46
VladRo26 <100710098+VladRo26@users.noreply.github.com>
2024-10-13 01:28:19 +0300

    installed ngx-bootstrap and fontawsome, made the link with the backend and display the list of companies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,22 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
   title = 'client';
+  benefcompanies: any;
+
+
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:7057/api/BeneficiaryCompanies').subscribe
+    ({
+      next: response => this.benefcompanies = response,
+      error: error => console.error(error)
+    });
+  }
 }
+
+
+
+
