e71ea1d5fa24f62e970450190fd0c4d4eaa49b94
VladRo26 <100710098+VladRo26@users.noreply.github.com>
2025-03-15 17:58:34 +0200

    retry

--- src/app/_guards/guards/unsaved-changes.guard.ts
+++ src/app/_guards/guards/unsaved-changes.guard.ts
@@ -2,7 +2,7 @@ import { CanDeactivateFn } from '@angular/router';
 import { UserappEditComponent } from '../../feautures/userapp/userapp-edit/userapp-edit.component';
 
 export const unsavedChangesGuard: CanDeactivateFn<UserappEditComponent> = (component, currentRoute, currentState, nextState) => {
-  if (component.isFormDirty()) {  // ✅ Check actual changes
+  if (component.isFormDirty()) {  
     return confirm('Are you sure you want to continue? Any unsaved changes will be lost.');
   }
   return true;
