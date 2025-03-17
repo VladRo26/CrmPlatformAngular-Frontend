d31a747d7af1516df2a208577a3d5641b8556971
VladRo26 <100710098+VladRo26@users.noreply.github.com>
2025-03-18 00:55:03 +0200

    published

--- src/app/_interceptors/loading.interceptor.ts
+++ src/app/_interceptors/loading.interceptor.ts
@@ -1,7 +1,8 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { LoadingService } from '../_services/loading.service';
-import { delay, finalize } from 'rxjs';
+import { delay, finalize, identity } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   const loadingService = inject(LoadingService);
@@ -9,7 +10,7 @@ export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   loadingService.loading();
 
   return next(req).pipe(
-    delay(1000),
+    (environment.production ? identity : delay(1000)),
     finalize(() => {
       loadingService.idle();
     })
