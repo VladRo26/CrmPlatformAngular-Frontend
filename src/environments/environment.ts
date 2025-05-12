069a493de43c01203aa2badf80a94b486df5d79f
VladRo26 <100710098+VladRo26@users.noreply.github.com>
2025-05-12 23:58:29 +0300

    edited

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: 'https://crmplatformapi.fly.dev/api/',
-    hubsUrl: 'hubs/',
+    hubsUrl: 'https://crmplatformapi.fly.dev/hubs/',
 };
